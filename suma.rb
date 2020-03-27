c07de99e61258a769c781a242ef3437b8cfe0ed7
Jonathan <jmedel.7@gmail.com>
2020-03-27 00:15:07 -0300

    suma done

---
+++ suma.rb
@@ -0,0 +1,52 @@
+
+
+puts "ingresa un numero para sumar sus lo que sea"
+
+num = gets.chomp.to_i
+
+sum = 0
+
+for num+n in expression [do]
+
+
+up_to = ARGV[0].to_i
+#######################################################
+iterator = 0
+sum = 0
+while iterator <) up_to do
+    sum += iterator
+    iterator += 1
+end
+
+puts sum
+
+#########################################################
+
+sum = 0 
+for i in (0..up to) do
+    sum +=
+end
+
+puts sum
+
+#########################################################
+
+sum = 0
+
+(0..up_to).each do |num|
+    sum += num
+end
+
+puts sum
+
+#########################################################
+
+iterator = 1 
+sum = 0
+
+up_to.times do
+    sum += iterator
+    iterator += 1
+end
+
+puts sum
